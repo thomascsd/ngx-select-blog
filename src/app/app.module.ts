3d951b1347eb2585805b729b12e23cf040b93edb
thomascsd <thomascsd808@gmail.com>
2020-07-05 22:01:29 +0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,10 @@ import { NgSelectModule } from '@ng-select/ng-select';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CityService } from './city.service';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, NgSelectModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, HomeComponent ],
+  declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers: [CityService]
 })
